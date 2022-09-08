943641180029e1f1f2575f2df96ca13d6aee379a
hao007 <haohaoalt@163.com>
2022-09-08 15:55:48 +0800

    0908

--- TrirdParty/melodic_install.sh
+++ TrirdParty/melodic_install.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
+###
+ # @Author: zhanghao
+ # @Date: 2022-09-08 14:49:57
+ # @LastEditTime: 2022-09-08 15:05:15
+ # @FilePath: /hao_2d_lidar_slam/TrirdParty/melodic_install.sh
+ # @Description: 
+### 
 
 # lesson3
 sudo apt-get install ros-melodic-csm
@@ -10,6 +17,7 @@ sudo apt-get install ros-melodic-laser-geometry
 sudo apt-get install ros-melodic-libg2o
 
 # install ceres-solver-1.13.0
+sudo apt install libgoogle-glog-dev 
 unzip ceres-solver-1.13.0.zip
 cd ceres-solver
 mkdir build
--- TrirdParty/noetic_install.sh
+++ TrirdParty/noetic_install.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
+###
+ # @Author: zhanghao
+ # @Date: 2022-09-08 14:49:57
+ # @LastEditTime: 2022-09-08 15:29:54
+ # @FilePath: /hao_2d_lidar_slam/TrirdParty/noetic_install.sh
+ # @Description: 
+### 
 
 # lesson3
 sudo apt-get install ros-noetic-csm
@@ -10,6 +17,8 @@ sudo apt-get install ros-noetic-laser-geometry
 sudo apt-get install ros-noetic-libg2o
 
 # install ceres-solver-1.13.0
+
+sudo apt install libgoogle-glog-dev 
 unzip ceres-solver-1.13.0.zip
 cd ceres-solver
 mkdir build
@@ -19,6 +28,8 @@ make
 sudo make install
 
 # intsall gtsam-4.0.2
+sudo apt-get install libtbb-dev
+sudo apt-get install intel-mkl-full
 cd ../../
 unzip gtsam-4.0.2.zip
 cd gtsam-4.0.2
